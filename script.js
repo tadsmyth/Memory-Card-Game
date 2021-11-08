b4d7053dd274eb09051576d5cb36c3383469ad84
tadsmyth <tadsmith7@gmail.com>
2021-11-08 16:04:10 -0600

    added the 3d flip effect

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("test")
+const cards = document.querySelectorAll('.card')
+
+function flipCard() {
+    // adds the 'flip' class to any clicked card and removes it if it has the class
+    this.classList.toggle('flip')
+    
+}
+
+cards.forEach(card => card.addEventListener('click' , flipCard))
